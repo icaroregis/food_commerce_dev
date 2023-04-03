355f664c0741c39814a4a909aca61bd14a9a56bd
icaro_regis <progalmeidaregis@outlook.com>
2023-04-02 22:30:00 -0300

    feat: criação dos snacks

--- src/components/Snack/index.tsx
+++ src/components/Snack/index.tsx
@@ -1,4 +1,5 @@
 import { Container } from './style'
+import { FiPlus } from 'react-icons/fi'
 
 interface SnacksProps {
   snacks: any[]
@@ -9,14 +10,16 @@ export function Snack({ snacks }: SnacksProps) {
     <Container>
       {snacks.map((snack) => {
         return (
-          <div key={snack.id} className='smack'>
+          <div key={snack.id} className='snack'>
             <h2>{snack.name}</h2>
             <img src={snack.image} alt={snack.name} />
             <p>{snack.description}</p>
 
             <div>
               <strong>{snack.price}</strong>
-              <button type='button'></button>
+              <button type='button'>
+                <FiPlus />
+              </button>
             </div>
           </div>
         )
--- src/components/Snack/style.ts
+++ src/components/Snack/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { darken } from 'polished'
 
 export const Container = styled.div`
   display: grid;
@@ -7,5 +8,65 @@ export const Container = styled.div`
 
   @media (max-width: 500px) {
     grid-template-columns: 1fr;
+  }
+
+  .snack {
+    position: relative;
+    background: ${({ theme }) => theme.colors.black};
+    padding: 1.75rem 1.5rem;
+    border-radius: 4px;
+
+    h2 {
+      margin-bottom: 0.75rem;
+      font-weight: 700;
+      font-size: 1.5rem;
+      align-items: center;
+    }
+
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 11.25rem;
+      border-radius: 4px;
+      margin-bottom: 3.75rem;
+    }
+
+    p {
+      font-size: 0.875rem;
+    }
+
+    div {
+      margin-top: 0.875rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      strong {
+        font-size: 2rem;
+        font-weight: 500;
+      }
+
+      button {
+        background: ${({ theme }) => theme.colors.red};
+        width: 3rem;
+        height: 3rem;
+        border: none;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        //icon do button
+        svg {
+          stroke: ${({ theme }) => theme.colors.white};
+          width: 1.5rem;
+          height: 1.5rem;
+        }
+
+        &:hover {
+          background: ${darken(0.1, '#AA2424')};
+        }
+      }
+    }
   }
 `
