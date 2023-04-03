3edb087542f7c28a83299cab531737d9acf0dc58
icaro_regis <progalmeidaregis@outlook.com>
2023-04-03 00:18:10 -0300

    feat: consumindo dados pelo json-server

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,36 +1,26 @@
-import { Container } from './style'
+import { useState, useEffect } from 'react'
 import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
 import { Snack } from '../../../components/Snack/index'
+import { getBurguers } from '../../../services/api'
 
 export default function Burgers() {
-  const data = [
-    {
-      id: 1,
-      snack: 'burger',
-      name: 'Mega',
-      description:
-        'O artesanal tamanho família recheado com três carnes suculentas, queijo e bacon.',
-      price: 25.5,
-      image: 'https://i.imgur.com/upjIUnG.jpg',
-    },
-    {
-      id: 2,
-      snack: 'burger',
-      name: 'Extra bacon',
-      description:
-        'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
-      price: 23.5,
-      image: 'https://i.imgur.com/B4J04AJ.jpg',
-    },
-  ]
+  const [burgers, setBurgers] = useState([])
+
+  useEffect(() => {
+    async function getUser() {
+      const response = await getBurguers()
+      setBurgers(response.data)
+    }
+
+    getUser()
+  }, [])
 
   return (
     <>
       <Head title='Hambúrgueres' description='O melhor Hamburguer de Fortaleza!!!' />
       <SnackTitle>Hambúrgueres</SnackTitle>
-
-      <Snack snacks={data}></Snack>
+      <Snack snacks={burgers}></Snack>
     </>
   )
 }
--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,34 +1,26 @@
+import { useState, useEffect } from 'react'
 import { Head } from '../../../components/Head'
 import { Snack } from '../../../components/Snack'
 import { SnackTitle } from '../../../components/SnackTitle'
+import { getPizzas } from '../../../services/api'
 
 export default function Pizzas() {
-  const data = [
-    {
-      id: 1,
-      snack: 'pizza',
-      name: 'Calabresa',
-      description:
-        'Pizza recheada com calabresa, cebola, mussarela, orégano e azeitona, tendo uma borda recheada com catupiry.',
-      price: 25,
-      image: 'https://i.imgur.com/5rjJGkV.jpg',
-    },
-    {
-      id: 2,
-      snack: 'pizza',
-      name: 'Portuguesa',
-      description:
-        'Pizza recheada com presunto, mussarela, ovo, cebola, azeitona, orégano, tomate e molho de tomate, tendo uma borda recheada com catupiry.',
-      price: 28.5,
-      image: 'https://i.imgur.com/WCoyGoI.png',
-    },
-  ]
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    async function getAllPizzas() {
+      const response = await getPizzas()
+      setPizzas(response.data)
+    }
+
+    getAllPizzas()
+  }, [])
 
   return (
     <>
       <Head title='Pizzas' description='As melhores pizzas de Fortaleza!!!' />
       <SnackTitle>Pizzas</SnackTitle>
-      <Snack snacks={data}></Snack>
+      <Snack snacks={pizzas}></Snack>
     </>
   )
 }
